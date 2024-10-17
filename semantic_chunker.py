210afa18f5fb5123dd59f4e1f0bee8b2a0c9aff9
gc-bianchi <72824902+gc-bianchi@users.noreply.github.com>
2024-10-17 15:55:02 -0600

    working text cleaner

--- semantic_chunker.py
+++ semantic_chunker.py
@@ -2,6 +2,17 @@ import re
 
 
 def preprocess_text(text):
-
     text = re.sub(r"\s+", " ", text)
     return text.strip()
+
+
+def main():
+    with open("./data/test_text.txt") as file:
+        text = file.read()
+
+    cleaned_text = preprocess_text(text)
+
+    print(cleaned_text)
+
+
+main()
