c07fa56884f21ffc9f4b3abec46f9da8c803d89b
gc-bianchi <72824902+gc-bianchi@users.noreply.github.com>
2024-10-17 20:46:11 -0600

    calc distances

--- semantic_chunker.py
+++ semantic_chunker.py
@@ -1,6 +1,7 @@
 import re
 import numpy as np
 from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 def preprocess_text(text):
@@ -28,15 +29,24 @@ def compute_embeddings(sentences):
     return embeddings
 
 
+def find_distances(embeddings):
+    distances = []
+    for i in range(1, len(embeddings)):
+        # Compute cosine similarity between consecutive sentence embeddings
+        similarity = cosine_similarity([embeddings[i - 1]], [embeddings[i]])[0][0]
+        distances.append(similarity)
+    return distances
+
+
 def main():
     with open("./data/test_text.txt") as file:
         text = file.read()
 
     cleaned_sentences = preprocess_text(text)
     embeddings = compute_embeddings(cleaned_sentences)
+    distances = find_distances(embeddings)
 
-    # print(cleaned_sentences)
-    print(embeddings[:3])
+    print(distances[:3])
 
 
 main()
